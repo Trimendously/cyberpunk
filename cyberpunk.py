0ea0175834aa3e0f9a95485dc3bc2063859be2b7
vagrant <vagrant@precise32.(none)>
2020-10-12 12:41:10 +0000

    The fixed weapon parsing. Currently takes a rarirty input and selects a random weapon of that rarity. Soon will add more indepth interface.

--- cyberpunk.py
+++ cyberpunk.py
@@ -11,8 +11,25 @@ import urllib.request
 import random
 
 
+def userInput():
+    InvalidInput = True
+    print("The rarities are as follows: poor, common, rare, excellent")
+    formatted_rarity = 'Empty'
+    while (InvalidInput):
+        rarity = input("Enter the rarity you desire: ")
+        formatted_rarity = rarity[:1].upper() + rarity[1:].lower()
+        options = ["Poor","Common","Rare","Excellent"]
+
+        for x in range(0,4):
+            if (options[x] == formatted_rarity):
+                InvalidInput = False
+                break
+hn
+    return str(formatted_rarity)
+
 
 def weapon_parser():
+
     # A comprehensive webpage containing ample amount of cyber punk weapon info
     url = "https://cyberpunk.fandom.com/wiki/Weapons_in_Cyberpunk_2020"
 
@@ -20,30 +37,20 @@ def weapon_parser():
     response = urllib.request.urlopen(request)
 
     soup = BeautifulSoup((response.read()), "lxml")
-    counter = 0
-    table = soup.findAll('table')
-    random_weapon = random.randint(0,len(table))
+    table = soup.findAll('table') #All tales on this website
+
+    rare = userInput()
+
 
     placeholder = True
-
-
-    print("The rarities are as follows: poor, common, rare, excellent")
-    rare = input("Enter the rarity you desire: ")
-    print(rare)
-    #rare = rare.lower()
-    print(rare)
-    options = ["poor","common","rare","excellent"]
-
-
     while(placeholder):
         random_weapon = random.randint(0,len(table))
         if (len(table) > random_weapon): #Defensive coding
             for row in table[random_weapon].findAll('tr'):
                 rarity = row.findAll('td')
 
-                if (len(rarity)>4):
-                    #print('Hi %s' %rarity[4].text)
-                    if ((rarity[4].get_text(strip=True)) == rare):
+                if (len(rarity)>4): #Defensive coding
+                    if ((rarity[4].get_text(strip=True)) == rare): #Strips to get rid of whitespace
                         placeholder = False
                         print(row.find('td').get_text(strip =True))
                         break
